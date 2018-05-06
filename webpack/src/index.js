437a87cafc2489fd81ea681e3737591856b04cc8
chenm <897785466@qq.com>
2018-05-06 17:01:55 +0800

    分离webpack配置文件

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,14 +2,26 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import printMe from './print.js';
+import { cube } from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
+  var pre = document.createElement('pre');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  pre.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+  element.appendChild(pre)
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
@@ -26,6 +38,7 @@ function component() {
 let element = component(); // Store the element to re-render on print.js changes
 document.body.appendChild(element);
 
+// 有一些loader能使HMR变得简单。详见文档
 if (module.hot) {
   module.hot.accept('./print.js', function() {
     console.log('Accepting the updated printMe module!');
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,24 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+     title: 'Production'
+   })
   ],
   output: {
     filename: '[name].bundle.js',
